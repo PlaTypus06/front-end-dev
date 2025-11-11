f6852d91170f6c8ee7639961517e24c7d62482c1
PlaTypus06 <ghiffarithoriq@gmail.com>
2025-11-12 01:56:26 +0700

    demo je es

--- JE_ES/function.js
+++ JE_ES/function.js
@@ -12,17 +12,19 @@ d = Number(d);
 let nilai = (a + b + c + d) / 4;
 let status;
 
-
-
 if (nilai < 60) {
-    status = "Tidak lulus";
-}else if (nilai >= 60 && nilai < 75) {
-    status = "Lulus rata rata";
+  status = "Tidak lulus";
+} else if (nilai >= 60 && nilai < 75) {
+  status = "Lulus rata rata";
 } else {
-    status = "Lulus dengan nilai bagus";
+  status = "Lulus dengan nilai bagus";
 }
 
 function myFunction() {
-    document.getElementById("demo").innerHTML =
-     "nilai rata rata kamu adalah : " + nilai + "<br>" + "dengan status : " + status;
+  document.getElementById("demo").innerHTML =
+    "nilai rata rata kamu adalah : " +
+    nilai +
+    "<br>" +
+    "dengan status : " +
+    status;
 }
